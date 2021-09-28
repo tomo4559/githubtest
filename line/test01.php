53bd1b50edf152ddd674b869b8ebbb1734187e53
tomo4559 <tomo4559@hotmail.com>
2021-09-28 16:01:38 +0900

    a

--- line/test01.php
+++ line/test01.php
@@ -15,7 +15,7 @@ if ($message->{"text"} == 'テスト') {
         'template' => [ 'type' => 'confirm', 'text' => '元気ですかー？',
             'actions' => [
                 [ 'type' => 'message', 'label' => 'replyToken', 'text' => $replyToken ],
-                [ 'type' => 'message', 'label' => 'jsonObj', 'text' => $jsonObj' ],
+                [ 'type' => 'message', 'label' => 'jsonObj', 'text' => 'jsonObj' ],
             ]
         ]
  ];
